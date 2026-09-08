ea7fce6aef034c23107d6f9488d4881d8d3c6a91
Mika Meyer <mimeyer@c2r12s4.42wolfsburg.de>
2026-09-08 20:54:09 +0200

    feat: basic validation model

---
+++ src/models.py
@@ -0,0 +1,20 @@
+from typing import Any
+
+from pydantic import BaseModel, Field, model_validator
+
+TYPES = {"string": str, "number": float}
+
+class	FuncDef(BaseModel):
+
+	name: str = Field(..., max_length=100)
+	description: str = Field(..., max_length=300)
+	params: dict[str, Any] = Field(...)
+	returns: Any = Field(default=None)
+
+	@model_validator(mode='before')
+	@classmethod
+	def set_params(cls, data: dict[str, str | dict]):
+		data["params"] = {}
+		for name, pack in data["parameters"].items():
+			data["params"][name] = TYPES[pack["type"]]
+		return data
