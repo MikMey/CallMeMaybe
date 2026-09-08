ed0c2fc5e7856cc0bd7e5955adfa89e20ddeff43
mikmey <mik-mey@outlook.de>
2026-09-08 03:39:10 +0200

    feat: simple read and validation

--- src/__main__.py
+++ src/__main__.py
@@ -33,10 +33,38 @@ class MaybeArgs:
 
 			self.args = self.parse_args()
 
+TYPES = {"string": str, "number": float}
+
+class	FuncDef(BaseModel):
+
+	name: str = Field(..., max_length=100)
+	description: str = Field(..., max_length=300)
+	params: dict[str, Any] = Field(...)
+	returns: Any = Field(default=None)
+
+	@model_validator(mode='before')
+	@classmethod
+	def set_params(cls, data: dict):
+		data["params"] = {}
+		for name, pack in data["parameters"].items():
+			data["params"][name] = TYPES[pack["type"]]
+		return data
 
 
 if __name__ == "__main__":
 	args = MaybeArgs()
-	print(args.functions_definition)
-	print(args.output)
-	print(args.input)
+
+	with open(args.functions_definition, "r") as file:
+		func_def = json.load(file)
+	with open(args.input, "r") as file:
+		prompts = json.load(file)
+
+	funcs: list[FuncDef] = []
+	for func in func_def:
+		curr = FuncDef.model_validate(func)
+		funcs.append(curr)
+		print(curr)
+	for prompt in prompts:
+		print(prompt)
+
+		
