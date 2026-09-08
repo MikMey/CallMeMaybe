accf6d452c35799c2bbc533b7e845279685277a9
Mika Meyer <mimeyer@c2r12s4.42wolfsburg.de>
2026-09-08 20:53:30 +0200

    feat: refractor to other files

--- src/__main__.py
+++ src/__main__.py
@@ -2,57 +2,10 @@ import sys
 from typing import Any, Optional
 import json
 
-from pydantic import BaseModel, model_validator, Field
-
-# from llm_sdk import Small_LLM_Model
-import argparse
-
-
-class MaybeArgs:
-
-	FLAGS = [
-		"--functions_definition",
-		"--input",
-		"--output"
-		]
-
-	def __init__(self) -> None:
-		args = self.Parser(self.FLAGS).args
-
-		self.functions_definition = args.functions_definition
-		self.input = args.input
-		self.output = args.output
-
-	class Parser(argparse.ArgumentParser):
-
-		def __init__(self, flags: list[str]):
-			super().__init__()
-
-			for flag in flags:
-				self.add_argument(flag, required=True, type=str)
-
-			self.args = self.parse_args()
-
-TYPES = {"string": str, "number": float}
-
-class	FuncDef(BaseModel):
-
-	name: str = Field(..., max_length=100)
-	description: str = Field(..., max_length=300)
-	params: dict[str, Any] = Field(...)
-	returns: Any = Field(default=None)
-
-	@model_validator(mode='before')
-	@classmethod
-	def set_params(cls, data: dict):
-		data["params"] = {}
-		for name, pack in data["parameters"].items():
-			data["params"][name] = TYPES[pack["type"]]
-		return data
-
+from . import parse_args, FuncDef
 
 if __name__ == "__main__":
-	args = MaybeArgs()
+	args = parse_args()
 
 	with open(args.functions_definition, "r") as file:
 		func_def = json.load(file)
@@ -66,5 +19,7 @@ if __name__ == "__main__":
 		print(curr)
 	for prompt in prompts:
 		print(prompt)
+
+	
 
 		
