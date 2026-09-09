d388b63fe31d9956f8615dc2e2b5faa95685a1eb
mikmey <mik-mey@outlook.de>
2026-09-09 05:22:57 +0200

    feat: basic instaciation and caching of LLM_Model; feat: introduces basic pipeline; fix: token accuarition improved; feat: adds personal en/- and decode

--- src/feedback.py
+++ src/feedback.py
@@ -1,47 +1,139 @@
-from typing import Any
+from typing import Any, Callable, Optional
+import sys
 
 import numpy as np
+import json
+import random
+import functools
+import time
 
 from llm_sdk import Small_LLM_Model
-from models import FuncDef
+from .models import FuncDef
+
+def Timer(func: Callable) -> Callable:
+	@functools.wraps(func)
+	def wrapper(*args, **kwargs):
+		start = time.time()
+		result = func(*args, **kwargs)
+		end = time.time()
+		print(f"Time: {end-start}")
+		return result
+	return wrapper
+
+MSG = "Your job is function calling. You will be given a prompt and must choose from one of the functions and declare their arguments. You need to return in the following Format: [function_name],{[Argument]:[Value]}" 
+
 
 class	FeedbackLoop():
-	prompt: str
-	name: str
-	param: dict[str, Any]
+	_model: Small_LLM_Model = None
+	_vocab: dict[str, int] = {}
+	_dec_vocab: list[str] = []
+	_funcdefs: list[FuncDef] = []
 
-	def __init__(self, prompt: str, funcdefs: list[FuncDef]):
+
+	def __init__(self, prompt: str, funcdefs: Optional[list[FuncDef]] = None, model: Optional[str] = "Qwen/Qwen3-0.6B"):
 		self.prompt = prompt
-		self.funcdefs = funcdefs
+		if FeedbackLoop._model is None:
 
-	def get_token(self):
-		new_llm = Small_LLM_Model()
-    
-		ids_tensor = new_llm.encode(self.prompt)
-		ids_list = ids_tensor[0].tolist()
+			FeedbackLoop._model = Small_LLM_Model(model_name=model)
+			vocab_file = FeedbackLoop._model.get_path_to_vocab_file()
+			with open(vocab_file, 'r') as file:
+				FeedbackLoop._vocab = json.load(file)
+			FeedbackLoop._dec_vocab = [None] * len(FeedbackLoop._vocab)
+			for key, value in FeedbackLoop._vocab.items():
+				FeedbackLoop._dec_vocab[value] = key
+			FeedbackLoop._funcdefs = funcdefs
 
-		print("llm")
-		logits = new_llm.get_logits_from_input_ids(ids_list)
+
+	def get_answer(self):
+
+		text: str = MSG
+		for func in self._funcdefs:
+			text += (
+				f"\n\nName: {func.name}" \
+				f"\nDescription: {func.description}"
+				"\nArgument:"
+			)
+			for name, types in func.params.items():
+				text += (
+					f" {name}:{types}, "
+				)
+			text += (
+				f"\nReturn: {func.returns}"
+			)
+		text += (
+			f"\n\nYour Prompt is: {self.prompt}\n"
+		)
+		for _ in range(25):
+			text += self._get_token(text)[0][0]
+		print (text)
 		
-		token = []
-		score = []
-	
-		print("decode")
-		for token_id, logit_score in enumerate(logits):
-			token_text = new_llm.decode([token_id])
-			token.append(token_text)
-			score.append(logit_score)
-		print("sort")
-		token = np.array(token)
-		score = np.array(score)
-		order = np.argsort(score, descending=True)
-		token = token[order]
-		score = score[order]
-		score = ((score - np.min(score)) / (np.max(score) - np.min(score)) * 100)
 
-		for i in range(10):
-			print(f"{str(token[i])!r}\t{round(score[i], 3)}%")
+
+	def _choose_token(self, text: str) -> str:
+		pass
+
+
+	def _get_token(self, text: str, top_k: int = 10) -> list[list[str | float]]:
+		"""
+		Prompt to receive next token
+
+		Args
+		------
+		top_k: int = 10
+			top elements to be returned
+		"""
+		token_ids = self.encode(text)
+
+		logits = self._model.get_logits_from_input_ids(token_ids)
+
+		scores = np.array(logits)
+		token_ids = np.argsort(scores, descending=True)[:top_k]
+		scores = scores[token_ids]
+
+		tokens = self.decode(token_ids)
+
+		if len(scores) > 1:
+			scores = ((scores - np.min(scores)) / (np.max(scores) - np.min(scores)) * 100)
+
+		# for i in range(top_k):
+			# print(f"{tokens[i]!r}\t{round(float(scores[i]), 3)}%")
+		return [tokens, scores]
+
+
+	@staticmethod
+	def encode(text: str, known: Optional[bool] = True) -> list[int]:
+		text = text.translate(str.maketrans({" ": "Ġ", "\n": "Ċ"}))
+		token_ids = []
+		i = 0
+		while i < len(text):
+			seq = text[i:]
+			while seq and seq not in FeedbackLoop._vocab:
+				seq = seq[:-1]
+			if seq not in FeedbackLoop._vocab:
+				sys.exit(f"skill issue {seq}")
+			token_ids.append(FeedbackLoop._vocab[seq])
+			i += len(seq)
+		return token_ids
+
+
+	@staticmethod
+	def decode(token_ids: list[int]) -> list[str]:
+		tokens = []
+		for token_id in token_ids:
+			token = FeedbackLoop._dec_vocab[int(token_id)]
+			tokens.append(
+				token.translate(str.maketrans({"Ġ": " ", "Ċ": "\n"}))
+				)
+		return tokens
+
 
 if __name__ == "__main__":
-	newloop = FeedbackLoop("how are your?", None)
-	newloop.get_token()
+	newloop = FeedbackLoop(MSG, None)
+	text = ""
+	start = time.time()
+	for i in range(100):
+		MSG += newloop.get_token(MSG, 1)
+	print(MSG)
+	end = time.time()
+	print(f"Time: {end-start}")
+
